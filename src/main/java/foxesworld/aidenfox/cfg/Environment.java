c985ae50ce56fbfed8b1ea29db930e3aff5a7467
Aiden <miomoor182@gmail.com>
2022-08-25 02:20:25 +0300

    1.8.0-SNAPSHOT fixes

--- src/main/java/foxesworld/aidenfox/cfg/Environment.java
+++ src/main/java/foxesworld/aidenfox/cfg/Environment.java
@@ -15,8 +15,7 @@ public class Environment {
 
     public static final String MODID = "foxesmod";
     public static final String NAME = "Hard Tactical tools";
-    public static final String VERSION = "1.5";
-
+    public static final String VERSION = "1.8.0-SNAPSHOT";
     public static final String CFGNAME = "hardtools.cfg";
     public static final String clientsideProxy = "foxesworld.aidenfox.proxy.ClientProxy";
     public static final String serversideProxy = "foxesworld.aidenfox.proxy.CommonProxy";
