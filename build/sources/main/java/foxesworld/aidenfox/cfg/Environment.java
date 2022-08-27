ed6466729ba0c34ceacfa52f22101e121ac4f4db
Aiden <miomoor182@gmail.com>
2022-08-27 15:49:02 +0300

    1,11,3-SNAPSHOT

--- build/sources/main/java/foxesworld/aidenfox/cfg/Environment.java
+++ build/sources/main/java/foxesworld/aidenfox/cfg/Environment.java
@@ -1,6 +1,6 @@
 package foxesworld.aidenfox.cfg;
 
-import foxesworld.aidenfox.world.structureGen.parser.StructureParser;
+import foxesworld.aidenfox.world.StructureGen.parser.StructureParser;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.util.SoundEvent;
@@ -14,7 +14,7 @@ public class Environment {
 
     public static final String MODID = "foxesmod";
     public static final String NAME = "Hard Tactical tools";
-    public static final String VERSION = "1.11.0-SNAPSHOT";
+    public static final String VERSION = "1.11.3-SNAPSHOT";
     public static final String CFGNAME = "hardtools.cfg";
     public static final String clientsideProxy = "foxesworld.aidenfox.proxy.ClientProxy";
     public static final String serversideProxy = "foxesworld.aidenfox.proxy.CommonProxy";
--- src/main/java/foxesworld/aidenfox/world/OreGen/OreGen.java
+++ src/main/java/foxesworld/aidenfox/world/OreGen/OreGen.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022  OreGen by FoxesWorld
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package foxesworld.aidenfox.world.OreGen;
 
 import net.minecraft.block.Block;
@@ -37,12 +53,15 @@ public class OreGen implements IWorldGenerator {
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider ) {
-        if (world.provider.getDimension() == 0) {
-            generateOverworld(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
-            debugSend("Generating " + this.oreName + " in overWorld at min " + this.oreMinHeight + " Max " + this.oreMaxHeight + " with veinSize " + this.oreVeinSize);
-        }
-        if (world.provider.getDimension() == -1) {
-            generateNether(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
+        switch (world.provider.getDimension()){
+            case 0:
+                generateOverworld(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
+                debugSend("Generating " + this.oreName + " in overWorld at min " + this.oreMinHeight + " Max " + this.oreMaxHeight + " with veinSize " + this.oreVeinSize);
+                break;
+
+            case -1:
+                generateNether(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
+                break;
         }
     }
 
